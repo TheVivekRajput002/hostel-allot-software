bfb2675d4f5595c0622ff7d676b98dd6918a8812
Yashwant Namdev <yashnamdevnwg@gmail.com>
2026-07-26 16:34:57 +0530

    fix: update prisma version and refactor student form submission logic
    - Downgraded prisma version from 7.9.0 to 7.8.0 in package.json.
    - Refactored submitForm function in student.controller.js to include:
      - Input validation for required fields.
      - Check for existing student applications.
      - Improved error handling and response messages.

--- backend/src/controllers/student.controller.js
+++ backend/src/controllers/student.controller.js
@@ -1,22 +1,56 @@
 import prisma from '../db/db.js'
 
-export const submitForm = async (req, res) => {
-    const body = {
-        "fullName": "Vivek Rajput",
-        "gender": "MALE",
-        "category": "OBC",
-        "jeeRollNumber": "JEE24010001",
-        "branch": "Information Technology",
-        "admissionYear": 2024,
-        "email": "vivek.rajput@example.com",
-        "mobileNumber": "9876543210",
-        "homeState": "Madhya Pradesh"
+
+const submitForm = async (req ,res)=>{
+    try{
+        const {fullName, gender,category, Dob,RollNo,MobileNo,Branch,HomeState,Pincode} = req.body;
+        if (
+            [fullName, gender,category, Dob,RollNo,MobileNo,Branch,HomeState,Pincode].some(
+                (field) => field?.trim() === ""
+            )
+        ) {
+            return res.status(400).json('Enter all fileds');
+        }
+        const existingStudent = await prisma.HostelForm.findUnique({
+            where: {
+                RollNo: RollNo,
+            },
+        });
+        if (existingStudent) {
+            return res.status(409).json("You have already applied.");
+        }
+        const newStudent = await prisma.HostelForm.create({
+            data: {
+                fullName:fullName,
+                gender:gender,
+                category:category,
+                Dob:Dob,
+                RollNo:RollNo,
+                MobileNo:MobileNo,
+                Branch:Branch,
+                HomeState:HomeState,
+                Pincode:Pincode
+            },
+        });
+        if(!newStudent){
+            return res.status(400).json("Error while saving Details.")
+        }
+        return res.status(200).json({
+            "Data: ":newStudent,
+            "message":"Student data saved."
+        })
+        
     }
+    catch(error) {
+        console.log(Error, error);
+        res.status(500).json({
+            message:'Internal Sever Error',
+            error:error.message
+        })
 
-    const result =  await prisma.hostelForm.create({
-        data: body
-    })
+   }
+}
 
-    console.log(result)
-
+export {
+    submitForm
 }
