120f24831330ed3e65b6a7bc28d31827db89a346
Yashwant Namdev <yashnamdevnwg@gmail.com>
2026-07-26 16:54:09 +0530

    fix: add morgan for logging and update API documentation for student form retrieval

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
 import studentRouter from './routes/student.js'
 
 dotenv.config()
 
 const app = express()
-
+app.use(morgan('dev'))
 app.get('/', (req, res) => {
     res.json({message: "Hello World"})
 })
